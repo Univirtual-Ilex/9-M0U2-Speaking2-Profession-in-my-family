34391d6005efa43117dc0bd260f6535b278589fc
alex <digger85@utp.edu.co>
2020-03-12 18:47:05 -0500

    no devuelve el sonido

--- src/components/PersonaHover/PersonaHover.jsx
+++ src/components/PersonaHover/PersonaHover.jsx
@@ -1,22 +1,67 @@
 //Import
-import React from 'react'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
 import styles from './PersonaHover_styles'
 // components
 import Tooltip from '../Tooltip'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 // Componente base
 const PersonaHover_base = ({profession, tooltip, ...props}) => {
+
     const [visible, mostrarTooltip] = useState(false)
+    const [isRunning, setRunning] = useState(false)
+    const [chunks, setChunks] = useState([])
+
+    const ilxAudio = useRef()
+    const handleClick = () => {
+        mostrarTooltip(!visible)
+    }
+
+
+    useEffect(()=>{
+
+            let capture = navigator.mediaDevices.getUserMedia({ audio: true })
+            capture.then( stream => {
+                const mediaRecorder = new MediaRecorder(stream)
+                const audioChunks = [...chunks];
+                    if(isRunning)
+                        {
+                            mediaRecorder.start()
+                            console.log(mediaRecorder)
+                            console.log(chunks)
+                        } else if (mediaRecorder.state === 'recording') {
+                            mediaRecorder.stop()
+                            console.log("SE DETUVO ?")
+                        }
+
+                    // Si hay data registra todo en audioChunks
+                    mediaRecorder.addEventListener("dataavailable", event => {
+                        audioChunks.push(event.data);
+                        setChunks(audioChunks)
+                    })
+                    
+                    mediaRecorder.addEventListener("stop", () => {
+                        const audioBlob = new Blob(audioChunks);
+                        const audioUrl = URL.createObjectURL(audioBlob);
+                        const audio = new Audio(audioUrl);
+                        audio.play();
+                        console.log('SONANDO')
+                    })
+
+            })
+
+
+    }, [isRunning, chunks])
+
 
     return (
         <div {...props}>
-                <div className={'person ' + profession } onClick={() => mostrarTooltip(!visible)}>
+                <div className={'person ' + profession } onClick={() => handleClick()}>
                 </div>
                 <div className='recorder'>
-                    <button className='btn-clean'>
+                    <button className='btn-clean' onClick={ () => setRunning(!isRunning) }>
                         <img src='./src/rec_btn_2.svg' alt='record your voice' />
                     </button>
                     <button className='btn-clean'>
@@ -26,6 +71,7 @@ const PersonaHover_base = ({profession, tooltip, ...props}) => {
                 <div>
                     <Tooltip visible={visible} onClick={() => mostrarTooltip(!visible)} > {tooltip} </Tooltip>
                 </div>
+                <audio id="player"  ref={ilxAudio} controls></audio>
         </div>
     )
 }
