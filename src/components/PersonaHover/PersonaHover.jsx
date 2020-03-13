e59359faa1756ceb2e1a604f7138d64f9081406f
alex-av <digger85@utp.edu.co>
2020-03-13 02:00:43 -0500

    Funciona el audio recorder

--- src/components/PersonaHover/PersonaHover.jsx
+++ src/components/PersonaHover/PersonaHover.jsx
@@ -7,53 +7,56 @@ import Tooltip from '../Tooltip'
 import { useState, useEffect } from 'react'
 
 
+
 // Componente base
 const PersonaHover_base = ({profession, tooltip, ...props}) => {
 
     const [visible, mostrarTooltip] = useState(false)
-    const [isRunning, setRunning] = useState(false)
+    // States Audio recorder 
+    const [isRecording, setRecording] = useState(false)
     const [chunks, setChunks] = useState([])
-
+    let [mediaRecorder, setMediaRecorder] = useState(false)
     const ilxAudio = useRef()
     const handleClick = () => {
         mostrarTooltip(!visible)
     }
 
 
+
+    const handleAudio = (status) => {
+        let audioChunks =[]
+
+        if (status === 'start') {
+                mediaRecorder.start()
+                setRecording(true)
+        } else if (status === 'stop') {
+                mediaRecorder.stop()
+                setRecording(false)
+        }
+        mediaRecorder.addEventListener("dataavailable", event => {
+            audioChunks.push(event.data);
+            setChunks(audioChunks)
+        })
+
+    }
+
+    const playAudio = () => {
+ 
+        const audioBlob = new Blob(chunks);
+        const audioUrl = URL.createObjectURL(audioBlob);
+        const audio = new Audio(audioUrl);
+        audio.play();
+        console.log('Playing')
+
+    }
+
     useEffect(()=>{
-
-            let capture = navigator.mediaDevices.getUserMedia({ audio: true })
-            capture.then( stream => {
-                const mediaRecorder = new MediaRecorder(stream)
-                const audioChunks = [...chunks];
-                    if(isRunning)
-                        {
-                            mediaRecorder.start()
-                            console.log(mediaRecorder)
-                            console.log(chunks)
-                        } else if (mediaRecorder.state === 'recording') {
-                            mediaRecorder.stop()
-                            console.log("SE DETUVO ?")
-                        }
-
-                    // Si hay data registra todo en audioChunks
-                    mediaRecorder.addEventListener("dataavailable", event => {
-                        audioChunks.push(event.data);
-                        setChunks(audioChunks)
-                    })
-                    
-                    mediaRecorder.addEventListener("stop", () => {
-                        const audioBlob = new Blob(audioChunks);
-                        const audioUrl = URL.createObjectURL(audioBlob);
-                        const audio = new Audio(audioUrl);
-                        audio.play();
-                        console.log('SONANDO')
-                    })
-
-            })
-
-
-    }, [isRunning, chunks])
+        let consulta = navigator.mediaDevices.getUserMedia({ audio: true })
+        consulta.then( audiostream => {
+            const media = new MediaRecorder(audiostream)
+            setMediaRecorder(media)
+        })
+    }, [])
 
 
     return (
@@ -61,17 +64,21 @@ const PersonaHover_base = ({profession, tooltip, ...props}) => {
                 <div className={'person ' + profession } onClick={() => handleClick()}>
                 </div>
                 <div className='recorder'>
-                    <button className='btn-clean' onClick={ () => setRunning(!isRunning) }>
+                    <button className='btn-clean' onClick={ () => handleAudio(isRecording ? 'stop' : 'start') }>
                         <img src='./src/rec_btn_2.svg' alt='record your voice' />
                     </button>
-                    <button className='btn-clean'>
+
+
+
+
+                    <button className='btn-clean' onClick={ playAudio }>
                         <img src='./src/play_btn_2.svg' alt='listen to your voice' />
                     </button>
                 </div>
                 <div>
                     <Tooltip visible={visible} onClick={() => mostrarTooltip(!visible)} > {tooltip} </Tooltip>
                 </div>
-                <audio id="player"  ref={ilxAudio} controls></audio>
+                {mediaRecorder.state + ''}
         </div>
     )
 }
